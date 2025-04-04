dee0e3ace3609224358415f985b7188f1b962fc6
hdvsarm <harrison.arms@gmail.com>
2025-04-03 22:02:14 -0500

    added char count and prettied up print function

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
-from stats import word_count
+from stats import word_count, char_count, dict_sort
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
 def main():
-    contents=get_book_text("books/frankenstein.txt")
+    book_path="books/frankenstein.txt"
+    contents=get_book_text(book_path)
     num_words=word_count(contents)
-    print(f"{num_words} words found in the document")
+    char_dict=char_count(contents)
+    sorted_chars=dict_sort(char_dict)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for i in sorted_chars:
+        if i["char"].isalpha():
+            print(f"{i["char"]}: {i["count"]}")
+    print("============= END ===============")
 
+
+    
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,29 @@
 def word_count(string):
     word_list=string.split()
     return len(word_list)
+
+
+def char_count(text):
+    char_dict={}
+    text=text.lower()
+    for i in text:
+        if i in char_dict:
+            char_dict[i]+=1
+        else:
+            char_dict[i]=1
+    
+    return char_dict
+
+def sort_on(dict):
+    return dict["count"]
+
+def dict_sort(dict):
+    dict_to_list=[]
+    for i in dict:
+        new_dict={}
+        new_dict["char"] = i
+        new_dict["count"] = dict[i]
+        dict_to_list.append(new_dict)
+    
+    dict_to_list.sort(reverse=True, key=sort_on)
+    return dict_to_list
